c9a1ed3ea0238220f39b99cd26c1f9134d8473ac
ArronBurris <46752697+ArronBurris@users.noreply.github.com>
2019-12-11 06:33:11 -0600

    First commit

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { IMovies } from './movies';
+import { MovieService } from './movies.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent implements OnInit{
+  pageTitle: string = "Movies now playing"
+  imageWidth: number = 300;
+  imageMargin: number = 5;
+  movies: IMovies[] = [ ];
+
+  constructor(private movieService: MovieService) {}
+
+   ngOnInit(){
+    this.movieService.getMovies().subscribe(movie => {
+      console.log(movie.results);
+      this.movies = movie.results;
+    })
+   }
+
+ 
+
+}
---
+++ src/app/movies/movies.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { IMovies } from './movies';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn:'root'
+})
+
+export class MovieService{
+    constructor(private http: HttpClient){}
+
+    movieUrl =  'https://api.themoviedb.org/3/movie/now_playing?api_key=09449180441e77d9f5b404c92c15225d&language=en-US&page=1';
+    getMovies(): Observable<IMovies[]> {
+        return this.http.get<IMovies[]>(this.movieUrl)
+
+        
+    }
+}
